9a4036c184a021bb48941e1b40f7330f3a2f2a62
Felipe Rocha <feliperochadf32@gmail.com>
2025-07-13 15:46:54 -0300

    Delete utils/send.js

--- utils/send.js
+++
@@ -1,7 +0,0 @@
-async function sendMessage(phone, text) {
-    console.log("Enviando mensagem para:", phone);
-    console.log("Mensagem:", text);
-    // Aqui você integra com a Evolution API para envio real
-}
-
-module.exports = { sendMessage };
